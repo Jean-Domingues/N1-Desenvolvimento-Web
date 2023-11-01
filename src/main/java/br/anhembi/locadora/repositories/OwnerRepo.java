698fd5f73500783f52a3b3cbab8f9aa7a4e16b34
Gabriel Nori <gabriel.nori@elogroup.com.br>
2023-10-31 21:09:36 -0300

    ⚰️ removes dead codes

--- src/main/java/br/anhembi/locadora/repositories/OwnerRepo.java
+++
@@ -1,9 +0,0 @@
-package br.anhembi.locadora.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.anhembi.locadora.models.Owner;
-
-public interface OwnerRepo extends JpaRepository<Owner, Long> {
-
-}
